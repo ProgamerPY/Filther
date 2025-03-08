187bbf07538001d791bc5b4e7654bb8400461793
ProgamerPY <victorritossantos@gmail.com>
2025-03-08 12:27:59 -0300

    update

---
+++ labels/text.py
@@ -0,0 +1,5 @@
+import customtkinter as ctk
+
+def create_labels (window):
+    label1 = ctk.CTkLabel(window,text="The Filther")
+    label1.grid(row=1, column=0)
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import customtkinter as ctk
 
+from labels.text import create_labels
+
+
 window = ctk.CTk()
 
 window.geometry("800x600")
 
 window.title("The Filther")
 
+create_labels(window)
+
 window.mainloop()
+
